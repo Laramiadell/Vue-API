bc89ed50befcf1ce68f226be7d280944f7b414d3
Laramiadell <71432940+Laramiadell@users.noreply.github.com>
2021-06-01 18:29:16 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="col-md-12">
+      <h1>Borrowed Books Record</h1>
+      <div class="titles">
+          <ul class="list-group">
+          <li class="list-group-item">
+            <a href="#" class="list-group-item-action"
+                @click.prevent="selectBorrow(list)">
+            <table class="table">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">Student ID</th>
+                  <th scope="col">Book ID</th>
+                  <th scope="col">Date Borrowed</th>
+                  <th scope="col">Date Returned</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="borrow in borrows" :key="borrow.id">
+                  <td>{{borrow.books_id}}</td>
+                  <td>{{borrow.students_id}}</td>
+                  <td>{{borrow.date_borrowed}}</td>
+                  <td>{{borrow.date_returned}}</td>
+                </tr>
+              </tbody>
+            </table>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-8">
+      <Borrow :borrow="selectedBorrow" v-if="selectedBorrow!=null" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+import Borrow from './components/Borrow.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Borrow
+  },
+  data() {
+    return {
+      borrows: [],
+      selectedBorrow: null
+    }
+  },
+  methods: {
+    async getBorrows() {
+      axios.get('https://laravel-apiproj.herokuapp.com/api/borrows')
+      .then(response => {
+        if(response.status==200) {
+          this.borrows = response.data;
+        }
+      })
+    },
+    selectBorrow(borrow) {
+      this.selectedBorrow = borrow
+    }
+  },
+  created() {
+    this.getBorrows()
   }
 }
 </script>
@@ -21,6 +77,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 10000px;
+  margin:30px;
 }
+
 </style>
---
+++ src/components/Borrow.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-if="borrow!=null">
+            <h1>{{borrow.student_id}}</h1>
+            <div>
+                {{borrow.books_id}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        borrow: null
+    }
+}
+</script>
+
+<style>
+</style>
